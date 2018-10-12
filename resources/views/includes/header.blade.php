e4bce1057653b949598a1cad6d6f6c0a8b896815
Rohan <rad59@njit.edu>
2018-10-12 14:59:56 -0400

    Task: Added the header and javascript footer file for the includes section of the page

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,9 @@
+<div class="navbar">
+    <div class="navbar-inner">
+        <a id="logo" href="/">IS601-Laravel</a>
+        <ul class="nav">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+        </ul>
+    </div>
+</div>
