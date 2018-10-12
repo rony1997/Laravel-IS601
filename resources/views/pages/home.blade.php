965d7cbb348fd0e5c9e51543ea3430e0686fb41c
Rohan <rad59@njit.edu>
2018-10-12 15:11:10 -0400

    Edit: Made changes in the home and about page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.landing')
 @section('content')
 
-    <h1> Hello World Welcome to my Larvel Page. This is my first Laravel Page. </h1>
+    <h1> Hello World ! This is my Laravel Home Page. </h1>
 
 @stop
